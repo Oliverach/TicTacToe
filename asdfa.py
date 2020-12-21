c87e5928ba44bc354499ed5e3b42fac88c571589
Oliverach <70696127+Oliverach@users.noreply.github.com>
2020-12-21 17:06:08 +0100

    Add files via upload

---
+++ asdfa.py
@@ -0,0 +1,21 @@
+def sdf():
+    if 1 == 1:
+        return 
+    if sdf() == None:
+        print("lol")
+
+
+
+# while True:
+#         logicBlockingO()
+#         if logicBlockingO() == None:
+#             logicPlacingO()
+#             if logicPlacingO() == None:
+#                 randomPlacingO()
+#             else:
+#                 logicPlacingO() == None
+#                 break
+#         else:
+#             logicBlockingO() == None
+#             break
+#     showGameboard()
